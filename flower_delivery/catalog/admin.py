e0e7af6f8803e64fe0a1c83e714f38046c6df89e
rsi2024 <164152459+rsi2024@users.noreply.github.com>
2024-11-18 22:56:16 +0300

    (flower)

--- flower_delivery/catalog/admin.py
+++ flower_delivery/catalog/admin.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from .models import Flower, Order
+from django.contrib.admin import AdminSite
 
 admin.site.register(Flower)
 admin.site.register(Order)
-from django.contrib import admin
+
+# Переопределение заголовка
+class MyAdminSite(AdminSite):
+    site_header = "Администрирование"
+    site_title = "Администрирование"
+    index_title = "Администрирование сайта"
+
+admin_site = MyAdminSite(name='myadmin')
 
 # Register your models here.
--- flower_delivery/flower_delivery/admin.py
+++ flower_delivery/flower_delivery/admin.py
@@ -4,3 +4,4 @@ from django.contrib import admin
 admin.site.site_header = "Администрирование сайта"
 admin.site.site_title = "Администрирование"
 admin.site.index_title = "Добро пожаловать в панель администрирования"
+
